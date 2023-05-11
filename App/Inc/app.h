3e45fdaddeaac07fccc66bbd93b7c4c0f2584d66
kramxkomitus <kramxkomitus@gmail.com>
2023-05-11 20:27:09 +0400

    add set up the project

--- App/Inc/app.h
+++ App/Inc/app.h
@@ -2,7 +2,17 @@
 #define INC_APP_H_
 
 #include "main.h"
+#include "stdbool.h"
+#include "ws2812.h"
+#include "tim.h"
+#include "dma.h"
+#include "stm32f1xx_it.h"
+
 
 void app();
+// void  USB_APP_Receive(uint8_t* Buf, uint32_t *Len);
 
+ 
 #endif /* INC_APP_H_ */
+
+
